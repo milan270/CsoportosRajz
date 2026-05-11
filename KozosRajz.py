864d6b5829ee84817234fdeb3125f90618ae8592
Pop <bsmilan975@gmail.com>
2026-05-11 08:38:20 +0200

    falomb

--- KozosRajz.py
+++ KozosRajz.py
@@ -3,3 +3,12 @@
 #lorant: fa torzse, hatter
 import turtle
 turtle.speed(0)
+i = 0
+while i < 12:
+    turtle.color("green")
+    turtle.begin_fill()
+    turtle.circle(60)
+    turtle.right(30)
+    turtle.end_fill()
+    i+= 1
+turtle.done()
