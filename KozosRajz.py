f2d0d952d23ca9c618915c742b603ab88f776182
Pop <bsmilan975@gmail.com>
2026-05-11 09:06:57 +0200

    fa goto

--- KozosRajz.py
+++ KozosRajz.py
@@ -1,7 +1,7 @@
 import turtle
 turtle.speed(0)
 i = 0
-
+turtle.goto(-30,-250)
 while i < 4:
     turtle.color("brown")
     turtle.begin_fill()
@@ -12,12 +12,12 @@ while i < 4:
     turtle.end_fill()
     i += 1
 i = 0
-
+turtle.goto(0,0)
 while i < 12:
     turtle.color("green")
     turtle.begin_fill()
     turtle.circle(60)
     turtle.right(30)
     turtle.end_fill()
-    i+ = 1
+    i += 1
 turtle.done()
