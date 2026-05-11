045b11ed98261eb6e00b51e2244618a538468a6e
Kacso Lorant <lorantkacso@gmail.com>
2026-05-11 08:38:51 +0200

    fatorzs

--- KozosRajz.py
+++ KozosRajz.py
@@ -1,4 +1,26 @@
 #milan: fa lomb
 #lorant fa torzs hatter
 import turtle
+
+i = 0
+
+
+
 turtle.speed(0)
+
+
+
+turtle.begin_fill()
+while i < 4:
+    turtle.color("brown")
+    turtle.begin_fill()
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(150)
+    turtle.left(90)
+    i += 1
+
+    turtle.end_fill()
+
+
+turtle.done()
